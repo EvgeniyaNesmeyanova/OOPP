8c0386f2a3d3a920ed46cd2197b3aae9d2494fa8
EvgeniyaNesmeyanova <prof_nea@mail.ru>
2024-03-07 19:01:00 +0500

    add missing functionality and tests

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,13 +1,14 @@
 public class Radio {
     private int currentStation;
-
-   // public void setToMaxStation() {  //устанавливаем максимальное значение станций
-   //     currentStation = 9;
-   // }
+    private int currentVolume;
 
     public int getCurrentStation() {
         return currentStation;
-    }// считываем состояние радиостанции
+    }   // считываем состояние радиостанции
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    } // считываем установленную громкость
 
     public void setCurrentStation(int newCurrentStation) {   //описываем порядок установки станции
         if (newCurrentStation < 0) { //если меньше 0, то оставляем станцию как есть
@@ -17,11 +18,38 @@ public class Radio {
                 return;
             } //если больше 9, то оставляем станцию как есть
             else {
-                currentStation = newCurrentStation;  //устанавливаем станцию
+                this.currentStation = newCurrentStation;  //устанавливаем станцию
             }
         }
 
     }
 
+    public void next() {  //описываем условия переключения станций вперед
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void prev() { //описываем условия переключения станций назад
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public void increaseVolume() { //описываем условия переключения громкости вперед
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() { //описываем условия переключения громкости назад
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
 
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -3,9 +3,9 @@ import org.junit.jupiter.api.Test;
 
 
 public class RadioTest {
-    //установить станцию из промежутка от 0 до 9
+
     @Test
-    public void shouldSetStation() {
+    public void shouldSetStation() { //проверить установку радиостанции с допустимым значением
         Radio station = new Radio();
 
         station.setCurrentStation(5);
@@ -71,5 +71,115 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void nextTest() {  //переключить на одну станцию вперед при установленном значении от 0 до 8
+        Radio station = new Radio();
 
+        station.setCurrentStation(5);
+        station.next();
+
+        int expected = 6;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nextTestMax() { //переключить на одну станцию вперед при установленном граничном значении 9
+        Radio station = new Radio();
+
+        station.setCurrentStation(9);
+        station.next();
+
+        int expected = 0;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prevTest() {  //переключить на одну станцию назад при установленном значении от 1 до 9
+        Radio station = new Radio();
+
+        station.setCurrentStation(5);
+        station.prev();
+
+        int expected = 4;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prevTestMin() {  //переключить на одну станцию назад при установленном граничном значении 0
+        Radio station = new Radio();
+
+        station.setCurrentStation(0);
+        station.prev();
+
+        int expected = 9;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void increaseVolumeTest() { //увеличить звук от нулевого значения
+        Radio station = new Radio();
+
+        for (int i = 0; i < 2; i++) {
+            station.increaseVolume();
+        }
+
+        int expected = 2;
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void increaseVolumeMaxTest() {//увеличить звук больше 100 раз
+        Radio station = new Radio();
+
+        for (int i = 0; i < 101; i++) {
+            station.increaseVolume();
+        }
+
+        int expected = 100;
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void decreaseVolumeMinTest() { //уменьшить звук от нулевого значения
+        Radio station = new Radio();
+
+        for (int i = 0; i < 2; i++) {
+            station.decreaseVolume();
+        }
+
+        int expected = 0;
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void decreaseVolumeTest() { //уменьшить звук после повышения
+        Radio station = new Radio();
+
+        for (int i = 0; i < 21; i++) {
+            station.increaseVolume();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            station.decreaseVolume();
+        }
+
+        int expected = 11;
+        int actual = station.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
